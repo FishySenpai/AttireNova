27312b2ef654d9defef239dc96c59dd65f157a33
FishySenpai <akanomanbasit@gmail.com>
2023-04-25 18:46:53 +0500

    display products

--- src/App.js
+++ src/App.js
@@ -7,24 +7,36 @@ function App() {
     const temp = await fetch("https://dummyjson.com/products").then((res) =>
       res.json()
     );
-    setProducts(temp);
+    setProducts(temp.products);
     console.log(temp);
   };
 
   useEffect(() => {
     FetchProducts();
     console.log(products);
-  }, [products]);
+  }, []);
 
   return (
     <div className="App">
       <div className="px-6 items-center mx-auto container justify-between">
         <div className="sm:p-6 pt-12 items-center container justify-between">
-          test
           <ul className="flex flex-wrap">
-            {products?.map((product) => (
-              <li>
-                {console.log(products)}
+            {products?.map((top, index) => (
+              <li className="mr-4 md:mr-8 pb-6 " key={top.id}>
+                <a href={`/topanime/${top.id}`}>
+                  <img
+                    className="w-[220px] h-[144px]  rounded hover:shadow-lg cursor-pointer hover:scale-105"
+                    src={top.thumbnail}
+                    alt="img"
+                  />
+                </a>
+                <div className="w-36 md:w-48 text-gray-500 text-lg hover:text-red-500 cursor-pointer">
+                  <button >
+                    
+                      {top.title}
+                    
+                  </button>
+                </div>
               </li>
             ))}
           </ul>

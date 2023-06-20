0287082811d4ab6ab5f9407ed4b98da2ef1d0bb2
FishySenpai <akanomanbasit@gmail.com>
2023-06-20 17:42:49 +0500

    added home page with new api

--- src/components/test.js
+++ src/components/test.js
@@ -1,8 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-
+import Products from "./Products";
+import { Link } from "react-router-dom";
 const Test = () => {
+    const [products, setProducts] = useState([]);
   useEffect(() => {
+    
     const fetchData = async () => {
       const options = {
         method: "GET",
@@ -22,15 +25,43 @@ const Test = () => {
       try {
         const response = await axios.request(options);
         console.log(response.data);
+        setProducts(response.data.payload.products);
       } catch (error) {
         console.error(error);
       }
+      
     };
-
     fetchData();
   }, []);
 
-  return <div>Fetching data...</div>;
+  return (
+    
+      <div>
+        <div className="px-6 items-center mx-auto container justify-between">
+          <div className="sm:p-6 pt-12 items-center container justify-between">
+            <ul className="flex flex-wrap">
+              {products?.map((top, index) => (
+                <li className="mr-4 md:mr-8 pb-6 " key={top.webID}>
+                  <a href={`/info/${top.webID}`}>
+                    <img
+                      className="w-[220px] h-[144px]  rounded hover:shadow-lg cursor-pointer hover:scale-105"
+                      src={top.image.url}
+                      alt="img"
+                    />
+                  </a>
+                  <div className="w-36 md:w-48 text-gray-500 text-lg hover:text-red-500 cursor-pointer">
+                    <button>
+                      <Link to={`/info/${top.webID}`}>{top.productTitle}</Link>
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    
+  );
 };
 
 export default Test;

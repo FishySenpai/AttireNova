24ed6ccf89cd32f7e38d58b6599b71dfb99c57c3
FishySenpai <akanomanbasit@gmail.com>
2023-07-25 17:16:23 +0500

    fixed address issue

--- src/components/User/CheckOut.js
+++ src/components/User/CheckOut.js
@@ -4,37 +4,17 @@ import { db } from "../firebaseConfig";
 import { auth } from "../firebaseConfig";
 import { doc, getDocs, collection, where, deleteDoc, setDoc } from "firebase/firestore";
 import { Link, useNavigate } from "react-router-dom";
+import ContactInfo from "./ContactInfo";
 const CheckOut = () => {
   const [data, setData] = useState([]); // data is returned back in []
   const [user, setUser] = useState({});
   const [subTotal, setSubTotal] = useState();
+  const [quantity, setQuantity] = useState(1);
+  const [showQuantity, setShowQuantity] = useState(false);
+  const [quantityPrice, setQuantityPrice] = useState();
   const navigate = useNavigate();
   const [thumbnail, setThumbnail] = useState();
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [address, setAddress] = useState("");
-  const [city, setCity] = useState("");
-  const [country, setCountry] = useState("");
-  const [zip, setZip] = useState("");
 
-  // State for payment information
-  const [card, setCard] = useState("");
-const info = {name, email, address, city, country, zip, card};
-  // Function to handle form submission
-  const handleSubmit = (e) => {
-    addFav();
-    e.preventDefault();
-    // Do something with the form data, e.g., send it to a server
-    console.log({
-      name,
-      email,
-      address,
-      city,
-      country,
-      zip,
-      card,
-    });
-  };
   
   useEffect(() => {
     onAuthStateChanged(auth, (currentUser) => {
@@ -54,29 +34,7 @@ const deleteFav = async (id) => {
   }
 };
 
- const addFav = async () => {
-   // Add a new document in collection "favs"
-   if (user) {
-     try {
-      const infoAsString = info.toString();
-       await setDoc(
-         doc(db, "users", user.uid, "info", name, {
-           name,
-           email,
-           address,
-           city,
-           country,
-           zip,
-           card,
-         })
-       );
-     } catch (err) {
-       console.log(err);
-     }
-   } else {
-     navigate("/login");
-   }
- };
+
 
   useEffect(() => {
     // ... (previous useEffect hooks) ...
@@ -110,6 +68,7 @@ const deleteFav = async (id) => {
         const docSnap = async () => {
           const fav = await getDocs(docRef);
           setData(fav.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+          setQuantityPrice(data.product.price.current.value)
           console.log(data);
         };
         docSnap();
@@ -118,132 +77,12 @@ const deleteFav = async (id) => {
       }
     }
   }, [user?.uid]); // use dependency list so useEffect only runs when there is change to useState
-  if (user) {
+
+
+  if (user && subTotal!==0) {
     return (
       <div className="flex flex-row ml-96">
-        <div class="leading-loose">
-          <div className="text-gray-500 text-2xl py-4 capitalize">
-            Contact Information
-          </div>
-          <form class="max-w-xl m-4 p-10 bg-white rounded shadow-xl">
-            <p class="text-gray-800 font-medium">Customer information</p>
-            <div class="">
-              <label class="block text-sm text-gray-00" for="cus_name">
-                Name
-              </label>
-              <input
-                class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded"
-                id="cus_name"
-                name="cus_name"
-                type="text"
-                required=""
-                placeholder="Your Name"
-                aria-label="Name"
-                onChange={(e) => setName(e.target.value)}
-              />
-            </div>
-            <div class="mt-2">
-              <label class="block text-sm text-gray-600" for="cus_email">
-                Email
-              </label>
-              <input
-                class="w-full px-5  py-4 text-gray-700 bg-gray-200 rounded"
-                id="cus_email"
-                name="cus_email"
-                type="text"
-                required=""
-                placeholder="Your Email"
-                aria-label="Email"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div class="mt-2">
-              <label class=" block text-sm text-gray-600" for="cus_email">
-                Address
-              </label>
-              <input
-                class="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
-                id="cus_email"
-                name="cus_email"
-                type="text"
-                required=""
-                placeholder="Street"
-                aria-label="Email"
-                onChange={(e) => setAddress(e.target.value)}
-              />
-            </div>
-            <div class="mt-2">
-              <label class="hidden text-sm  text-gray-600" for="cus_email">
-                City
-              </label>
-              <input
-                class="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
-                id="cus_email"
-                name="cus_email"
-                type="text"
-                required=""
-                placeholder="City"
-                aria-label="Email"
-                onChange={(e) => setCity(e.target.value)}
-              />
-            </div>
-            <div class="inline-block mt-2 w-1/2 pr-1">
-              <label class="hidden  text-sm text-gray-600" for="cus_email">
-                Country
-              </label>
-              <input
-                class="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
-                id="cus_email"
-                name="cus_email"
-                type="text"
-                required=""
-                placeholder="Country"
-                aria-label="Email"
-                onChange={(e) => setCountry(e.target.value)}
-              />
-            </div>
-            <div class="inline-block mt-2 -mx-1 pl-1 w-1/2">
-              <label class="hidden  text-sm text-gray-600" for="cus_email">
-                Zip
-              </label>
-              <input
-                class="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
-                id="cus_email"
-                name="cus_email"
-                type="text"
-                required=""
-                placeholder="Zip"
-                aria-label="Email"
-                onChange={(e) => setZip(e.target.value)}
-              />
-            </div>
-            <p class="mt-4 text-gray-800 font-medium">Payment information</p>
-            <div class="">
-              <label class="block text-sm text-gray-600" for="cus_name">
-                Card
-              </label>
-              <input
-                class="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
-                id="cus_name"
-                name="cus_name"
-                type="text"
-                required=""
-                placeholder="Card Number MM/YY CVC"
-                aria-label="Name"
-                onChange={(e) => setCard(e.target.value)}
-              />
-            </div>
-            <div class="mt-4">
-              <button
-                class="px-4 py-1 text-white font-light tracking-wider bg-gray-900 rounded"
-                type="submit"
-                onClick={handleSubmit}
-              >
-                Pay: ${subTotal + 5}
-              </button>
-            </div>
-          </form>
-        </div>
+        <ContactInfo user={user} subTotal={subTotal} />
         <div className="px-6 items-center mx-auto container justify-between">
           <div className="sm:p-6 pt-12 items-center container justify-between">
             <div className="text-gray-500 text-2xl py-4 capitalize mr-96">
@@ -281,7 +120,11 @@ const deleteFav = async (id) => {
                           <div className="mt-1">
                             {top.product.variants[0]?.colour}
                           </div>
-                          <div className="ml-6 mt-1">Qty: {top.quantity}</div>
+                          <div className="flex flex-row px-2 mb-2 font-mono">
+                            <div className="text-[20px] text-gray-800 ml-10">
+                              Quantity:{" "}
+                            </div>
+                          </div>
                         </div>
                         <div className="mt-1">{top.size}</div>
 
---
+++ src/components/User/ContactInfo.js
@@ -0,0 +1,189 @@
+import React, {useState} from 'react'
+import { db } from "../firebaseConfig";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  doc,
+  getDocs,
+  collection,
+  where,
+  deleteDoc,
+  setDoc,
+} from "firebase/firestore";
+const ContactInfo = ({user, subTotal}) => {
+      const [name, setName] = useState("");
+      const [email, setEmail] = useState("");
+      const [address, setAddress] = useState("");
+      const [city, setCity] = useState("");
+      const [country, setCountry] = useState("");
+      const [zip, setZip] = useState("");
+      // State for payment information
+      const [card, setCard] = useState("");
+        const navigate = useNavigate();
+      // Function to handle form submission
+      const handleSubmit = (e) => {
+        addFav();
+        e.preventDefault();
+        // Do something with the form data, e.g., send it to a server
+        console.log({
+          name,
+          email,
+          address,
+          city,
+          country,
+          zip,
+          card,
+        });
+      };
+
+       const addFav = async () => {
+         // Add a new document in collection "favs"
+         if (user) {
+           try {
+             await setDoc(doc(db, "users", user.uid, "info", user.uid), {
+               name,
+               email,
+               address,
+               city,
+               country,
+               zip,
+               card,
+             });
+           } catch (err) {
+             console.log(err);
+           }
+         } else {
+           navigate("/login");
+         }
+       };
+  
+  return (
+    <div>
+      {" "}
+      <div class="leading-loose">
+        <div className="text-gray-500 text-2xl py-4 capitalize">
+          Contact Information
+        </div>
+        <form class="max-w-xl m-4 p-10 bg-white rounded shadow-xl">
+          <p class="text-gray-800 font-medium">Customer information</p>
+          <div class="">
+            <label class="block text-sm text-gray-00" for="cus_name">
+              Name
+            </label>
+            <input
+              class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded"
+              id="cus_name"
+              name="cus_name"
+              type="text"
+              required=""
+              placeholder="Your Name"
+              aria-label="Name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div class="mt-2">
+            <label class="block text-sm text-gray-600" for="cus_email">
+              Email
+            </label>
+            <input
+              class="w-full px-5  py-4 text-gray-700 bg-gray-200 rounded"
+              id="cus_email"
+              name="cus_email"
+              type="text"
+              required=""
+              placeholder="Your Email"
+              aria-label="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div class="mt-2">
+            <label class=" block text-sm text-gray-600" for="cus_email">
+              Address
+            </label>
+            <input
+              class="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
+              id="cus_email"
+              name="cus_email"
+              type="text"
+              required=""
+              placeholder="Street"
+              aria-label="Email"
+              onChange={(e) => setAddress(e.target.value)}
+            />
+          </div>
+          <div class="mt-2">
+            <label class="hidden text-sm  text-gray-600" for="cus_email">
+              City
+            </label>
+            <input
+              class="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
+              id="cus_email"
+              name="cus_email"
+              type="text"
+              required=""
+              placeholder="City"
+              aria-label="Email"
+              onChange={(e) => setCity(e.target.value)}
+            />
+          </div>
+          <div class="inline-block mt-2 w-1/2 pr-1">
+            <label class="hidden  text-sm text-gray-600" for="cus_email">
+              Country
+            </label>
+            <input
+              class="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
+              id="cus_email"
+              name="cus_email"
+              type="text"
+              required=""
+              placeholder="Country"
+              aria-label="Email"
+              onChange={(e) => setCountry(e.target.value)}
+            />
+          </div>
+          <div class="inline-block mt-2 -mx-1 pl-1 w-1/2">
+            <label class="hidden  text-sm text-gray-600" for="cus_email">
+              Zip
+            </label>
+            <input
+              class="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
+              id="cus_email"
+              name="cus_email"
+              type="text"
+              required=""
+              placeholder="Zip"
+              aria-label="Email"
+              onChange={(e) => setZip(e.target.value)}
+            />
+          </div>
+          <p class="mt-4 text-gray-800 font-medium">Payment information</p>
+          <div class="">
+            <label class="block text-sm text-gray-600" for="cus_name">
+              Card
+            </label>
+            <input
+              class="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
+              id="cus_name"
+              name="cus_name"
+              type="text"
+              required=""
+              placeholder="Card Number MM/YY CVC"
+              aria-label="Name"
+              onChange={(e) => setCard(e.target.value)}
+            />
+          </div>
+          <div class="mt-4">
+            <button
+              class="px-4 py-1 text-white font-light tracking-wider bg-gray-900 rounded"
+              type="submit"
+              onClick={handleSubmit}
+            >
+              Pay: ${subTotal + 5}
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ContactInfo
